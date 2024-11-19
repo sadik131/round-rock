091a46b13481f93594405a5dd02c09a57c43b4df
supto <salatussadik131@gmail.com>
2024-11-20 02:19:01 +0530

    header and banner

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '0px',
+      screens: {
+        'lg': '1366px',
+      },
+    },
+    letterSpacing: {
+      '1': '8px',
+    },
+    extend: {
+      colors: {
+        primary: "#4D6BB9",
+        secondary:"#9FB1DE",
+        gray:"#4F4F4F",
+      },
+      fontFamily:{
+        poppin: ["Poppins", "sans-serif"]
+      }
+    },
+    plugins: [],
+  }
+
+}
